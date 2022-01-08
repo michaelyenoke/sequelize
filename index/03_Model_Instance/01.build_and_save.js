52d0c7747fc68736642275151781292bfba0d4c9
Y.H. Chen <michaelyenoke@hotmail.com>
2022-01-08 19:00:24 +0800

    Create 01.build_and_save.js

---
+++ index/03_Model_Instance/01.build_and_save.js
@@ -0,0 +1,71 @@
+const Sequelize = require('sequelize')
+const { DataTypes } = Sequelize
+const sequelize = new Sequelize('sequelize_project',  'michaelyenoke', '1qaz2wsx3edc', {
+    dialect:  'mysql',
+    host:'database-1.cqwpdgcki6p6.ap-southeast-1.rds.amazonaws.com',
+    port:3306
+})
+
+
+
+
+
+
+const Customer = sequelize.define('customer', {
+    user_id: {
+        type:DataTypes.INTEGER,
+        primaryKey:true,
+        autoIncrement:true
+    },
+    username:{
+        type: DataTypes.STRING,
+        allowNull:false
+    },
+    password:{
+        type: DataTypes.STRING,
+    },
+    age: {
+        type: DataTypes.INTEGER,
+        defaultValue:21
+    },
+    vegetarian:{
+        type: DataTypes.BOOLEAN,
+        defaultValue:true
+    }
+},
+{   
+ 
+    freezeTableName:true,
+    timestamps:false
+    
+});
+
+
+
+Customer.sync({ alter:true }).then(() => {
+    // working with our updated table.
+    // Inserting Data with Build() and Save()
+    const customer = Customer.build({ username:'Michaelyenoke', password:'12345678', age:30, Vegetarian:true});
+    //console.log(customer.username);
+    //console.log(customer.password);
+    
+
+    // add conditions
+     if (customer.age > 15){
+         customer.old = true;
+    }
+
+    // direct change the data above    
+    customer.username = 'soccer2'
+
+    // save into our database
+    return customer.save()
+
+    // JS code is executed in an event loop on a single thread. 
+    // Meaning only one thing can happen at a time.
+    // However, you can execute things asynchronously(callbacks, promises) so you can perform other actions without blocking the main thread.
+}).then((data) => {
+    console.log("User added to database!");
+}).catch((err) => {
+   
+});
