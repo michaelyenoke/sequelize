5bc89b0d4744ec8ba681c12299018bbbd8c27eb4
Y.H. Chen <michaelyenoke@hotmail.com>
2022-01-08 20:00:08 +0800

    Create 04.destory.js

---
+++ index/03_Model_Instance/04.destory.js
@@ -0,0 +1,63 @@
+const Sequelize = require('sequelize')
+const { DataTypes } = Sequelize
+const sequelize = new Sequelize('sequelize_project',  'michaelyenoke', '1qaz2wsx3edc', {
+    dialect:  'mysql',
+    host:'database-1.cqwpdgcki6p6.ap-southeast-1.rds.amazonaws.com',
+    port:3306
+})
+
+
+
+const Customer = sequelize.define('customer', {
+    user_id: {
+        type:DataTypes.INTEGER,
+        primaryKey:true,
+        autoIncrement:true
+    },
+    username:{
+        type: DataTypes.STRING,
+        allowNull:false
+    },
+    password:{
+        type: DataTypes.STRING,
+    },
+    age: {
+        type: DataTypes.INTEGER,
+        defaultValue:21
+    },
+    vegetarian:{
+        type: DataTypes.BOOLEAN,
+        defaultValue:true
+    }
+},
+{   
+    freezeTableName:true,
+    timestamps:false
+});
+
+
+
+Customer.sync({ alter:true })
+.then(() => {
+    return Customer.create({
+        username:'Mickey',
+        password:'7654321',
+        age:33,
+        vegetarian:false
+    })
+}).then((data) => {
+    console.log("User added to database!");
+
+    data.username = 'pizza'
+  
+    //Executing (default): DELETE FROM `customer` WHERE `user_id` = 12
+    return data.destroy()
+
+}).then((data) => {
+
+    console.log("Customer destoryed")
+    console.log(data.toJSON())
+    
+}).catch((err) => {
+   console.log('wrong')
+});
