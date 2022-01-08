4f0929efa093b4d48671de3cac781e6de1962ee3
Y.H. Chen <michaelyenoke@hotmail.com>
2022-01-08 15:33:34 +0800

    Update 01_connect.js

--- index/01_connect.js
+++ index/01_connect.js
@@ -1,5 +1,6 @@
 const Sequelize = require('sequelize')
 
+// connect to mySQL database created earler: 'sequelize_project'
 const sequelize = new Sequelize('sequelize_project',  'michaelyenoke', '1qaz2wsx3edc', {
     dialect:  'mysql',
     host:'database-1.cqwpdgcki6p6.ap-southeast-1.rds.amazonaws.com',
