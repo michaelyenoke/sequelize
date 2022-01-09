51bf054612d2f8dbe1849438da730fb9e7fc8791
Y.H. Chen <michaelyenoke@hotmail.com>
2022-01-09 21:34:11 +0800

    Create 15.Selecting_Rows_with_Operators_3_lt_eq.js

---
+++ index/04_Model_Querying/15.Selecting_Rows_with_Operators_3_lt_eq.js
@@ -0,0 +1,71 @@
+const Sequelize = require('sequelize')
+
+// Selecting Rows with Operators
+const { DataTypes, Op } = Sequelize
+
+
+const sequelize = new Sequelize('your_database',  'your_username', 'your_password', {
+    dialect:  'mysql',
+    host:'your_host',
+    port:3306
+})
+
+
+
+const Customer = sequelize.define('customer', {
+    user_id: {
+        type:DataTypes.INTEGER,
+        primaryKey:true,
+        autoIncrement:true
+    },
+    username:{
+        type: DataTypes.STRING,
+        allowNull:false,
+        validate:{
+            len:[3,6]
+        }
+    },
+    password:{
+        type: DataTypes.STRING,
+    },
+    age: {
+        type: DataTypes.INTEGER,
+        defaultValue:21
+    },
+    vegetarian:{
+        type: DataTypes.BOOLEAN,
+        defaultValue:true
+    }
+},
+{   
+    freezeTableName:true,
+    timestamps:false
+});
+
+
+
+
+Customer.sync({ alter:true })
+.then(() => {
+    // working with our update table.
+    // lt : less tnan
+    // eq :equal
+    return Customer.findAll({ where:{
+        age:{
+            [Op.or]:{
+                [Op.lt]:33,
+                [Op.eq]:null
+            }
+        }
+    }
+        
+    });
+}).
+then((data) => { // data in this case (bulk) is an array
+    data.forEach((element) => { // element is the object we want
+    console.log(element.toJSON())
+    })
+})
+.catch((err) => {
+   console.log(err)
+})
