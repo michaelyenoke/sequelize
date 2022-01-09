0d4de7efee40b16613278928525f1a9ba2a6f1b3
Y.H. Chen <michaelyenoke@hotmail.com>
2022-01-09 22:29:17 +0800

    Create 19.Deleting_Rows_with_Destroy().js

---
+++ index/04_Model_Querying/19.Deleting_Rows_with_Destroy().js
@@ -0,0 +1,56 @@
+const Sequelize = require('sequelize')
+
+// Selecting Rows with Operators
+const { DataTypes, Op } = Sequelize
+
+const sequelize = new Sequelize('your_database',  'your_username', 'your_password', {
+    dialect:  'mysql',
+    host:'your_host',
+    port:3306
+})
+
+
+
+const Customer = sequelize.define('customer', {
+    user_id: {
+        type:DataTypes.INTEGER,
+        primaryKey:true,
+        autoIncrement:true
+    },
+    username:{
+        type: DataTypes.STRING,
+    },
+    password:{
+        type: DataTypes.STRING,
+    },
+    age: {
+        type: DataTypes.INTEGER,
+        defaultValue:21
+    },
+    vegetarian:{
+        type: DataTypes.BOOLEAN,
+        defaultValue:true
+    }
+},
+{   
+    freezeTableName:true,
+    timestamps:false
+});
+
+
+
+
+Customer.sync({ alter:true })
+.then(() => {
+    // working with our update table.
+    // Executing (default): DELETE FROM `customer` WHERE `username` = 'hello!'
+    // 26
+    return Customer.destroy({where:{ username:'hello!'}})
+
+}).
+then((data) => { 
+    console.log(data)
+})
+.catch((err) => {
+   console.log(err)
+})
