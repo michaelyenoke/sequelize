e26aafc561ed03833c833b252784a13ffce8ccbb
Y.H. Chen <michaelyenoke@hotmail.com>
2022-01-09 17:31:52 +0800

    Create 05.Aggregations_with_Sequelize.fn()_AVG.js

---
+++ index/04_Model_Querying/05.Aggregations_with_Sequelize.fn()_AVG.js
@@ -0,0 +1,57 @@
+const { DataTypes } = Sequelize
+const sequelize = new Sequelize('your_database',  'your_username', 'your_password', {
+    dialect:  'mysql',
+    host:'your_host',
+    port:3306
+})
+
+
+
+const Customer = sequelize.define('customer', {
+    user_id: {
+        type:DataTypes.INTEGER,
+        primaryKey:true,
+        autoIncrement:true
+    },
+    username:{
+        type: DataTypes.STRING,
+        allowNull:false,
+        validate:{
+            len:[3,6]
+        }
+    },
+    password:{
+        type: DataTypes.STRING,
+    },
+    age: {
+        type: DataTypes.INTEGER,
+        defaultValue:21
+    },
+    vegetarian:{
+        type: DataTypes.BOOLEAN,
+        defaultValue:true
+    }
+},
+{   
+    freezeTableName:true,
+    timestamps:false
+});
+
+
+
+
+Customer.sync({ alter:true })
+.then(() => {
+    // working with our update table.
+    // SELECT AVG(`age`) AS `howOld` FROM `customer` AS `customer`;
+    // { howOld: '29.0385' }
+    return Customer.findAll({ attributes:[[sequelize.fn('AVG', sequelize.col('age')),'howOld']]});
+}).
+then((data) => { // data in this case (bulk) is an array
+    data.forEach((element) => { // element is the object we want
+    console.log(element.toJSON())
+    })
+})
+.catch((err) => {
+   console.log(err)
+})
