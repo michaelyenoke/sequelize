c872b1eb45dfb5868c9bd1a67bd943b74617a3e4
Y.H. Chen <michaelyenoke@hotmail.com>
2022-01-10 16:01:02 +0800

    Create 02.Validators.js

---
+++ index/08_ Validators_and_Constraints/02.Validators.js
@@ -0,0 +1,97 @@
+const Sequelize = require('sequelize')
+const bcrypt = require('bcrypt')
+const zlib = require('zlib')
+
+const { DataTypes, Op } = Sequelize
+
+const sequelize = new Sequelize('sequelize_project',  'michaelyenoke', '1qaz2wsx3edc', {
+    dialect:  'mysql',
+    host:'database-1.cqwpdgcki6p6.ap-southeast-1.rds.amazonaws.com',
+    port:3306
+})
+
+
+
+const Customer = sequelize.define('customer', {
+    user_id: {
+        type:DataTypes.INTEGER,
+        primaryKey:true,
+        autoIncrement:true
+    },
+    username:{
+        type: DataTypes.STRING,
+        allowNull:false,
+        validate:{
+            len:[1,10]
+        },
+        // get() {
+        //     const rawValue = this.getDataValue('username')
+        //     return rawValue.toUpperCase()
+        // }
+    },
+    password:{
+        type: DataTypes.STRING,
+        // set(value) {
+        //     const salt = bcrypt.genSaltSync(12)
+        //     const hash = bcrypt.hashSync(value, salt)
+        //     this.setDataValue('password', hash)
+        // }
+    },
+    age: {
+        type: DataTypes.INTEGER,
+        defaultValue:21
+    },
+    vegetarian:{
+        type: DataTypes.BOOLEAN,
+        defaultValue:true
+    },
+    description: {
+        type:DataTypes.STRING,
+        // set(value){
+        //     const compressed = zlib.deflateSync(value).toString('base64')
+        //     this.setDataValue('description', compressed)
+        // },
+        // get() {
+        //     const value = this.getDataValue('description')
+        //     const uncompressed = zlib.inflateSync(Buffer.from(value, 'base64'))
+        //     return uncompressed.toString()
+        // }    
+    },
+    aboutUser:{
+        type:DataTypes.VIRTUAL,
+        get(){
+            return this.username+this.description
+        }
+    },
+    email:{
+        type:DataTypes.STRING,
+        // Validators
+        validate:{
+            isEmail: true
+        }
+
+    }    
+},
+{   
+    freezeTableName:true,
+    timestamps:false
+});
+
+
+
+Customer.sync({ alter:true })
+.then(() => {
+    return Customer.create({
+        username:'Jisoo2',
+        password:'12345678',
+        email:'hello'  // put something is not email to test
+        //   ValidationErrorItem {
+        //   message: 'Validation isEmail on email failed',
+        //   ...
+        //   }
+    })
+}).then((data) => {
+    console.log(data.toJSON())
+}).catch((err) => {
+   console.log(err)
+})
