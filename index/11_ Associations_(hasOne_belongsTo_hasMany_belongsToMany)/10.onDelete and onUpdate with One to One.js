802d9f03c4f3e19b513de56b34368eb18baa3483
Y.H. Chen <michaelyenoke@hotmail.com>
2022-01-11 23:05:32 +0800

    Create 10.onDelete and onUpdate with One to One.js

---
+++ index/11_ Associations_(hasOne_belongsTo_hasMany_belongsToMany)/10.onDelete and onUpdate with One to One.js
@@ -0,0 +1,50 @@
+const Sequelize = require('sequelize')
+const bcrypt = require('bcrypt')
+const zlib = require('zlib')
+const { HasOne } = require('sequelize')
+
+const { DataTypes, Op } = Sequelize
+
+const sequelize = new Sequelize('sequelize_project',  'michaelyenoke', '1qaz2wsx3edc', {
+    dialect:  'mysql',
+    host:'database-1.cqwpdgcki6p6.ap-southeast-1.rds.amazonaws.com',
+    port:3306
+})
+
+// table one : country
+const Country = sequelize.define('country', {
+    countryName:{
+        type:DataTypes.STRING,
+        unque:true
+    }
+},{
+    timestamps:false
+})
+
+// talbe two : capital
+const Capital = sequelize.define('capital',{
+    capitalName:{
+        type:DataTypes.STRING,
+        unique:true
+    }
+},{
+    timestamps: false
+})
+
+// onDelete and onUpdate with One to One
+// CASCADE 任何相關的表格
+Country.hasOne(Capital, { onUpdate: 'CASCADE'}); 
+Capital.belongsTo(Country,{ onDelete: 'CASCADE'});
+
+
+let country, capital;
+
+sequelize.sync({alter:true}).then(()=> {
+    // working with our updated table.
+    return Country.destroy({ where: { countryName: 'Spain' }})
+}).then((data) => {
+    console.log(data)
+})
+.catch((err) => {
+    console.log(err)
+})
