4d3cf729c4e53cb93f12d937bfee0746ce7323b8
Y.H. Chen <michaelyenoke@hotmail.com>
2022-01-11 10:07:35 +0800

    Create 03.Restoring_a_Soft_Deleted_Record_with_Restore().js

---
+++ index/10_Paranoid_Tables.js/03.Restoring_a_Soft_Deleted_Record_with_Restore().js
@@ -0,0 +1,115 @@
+const Sequelize = require('sequelize')
+const bcrypt = require('bcrypt')
+const zlib = require('zlib')
+
+const { DataTypes, Op } = Sequelize
+
+const sequelize = new Sequelize('sequelize_project',  'michaelyenoke', '1qaz2wsx3edc', {
+    dialect:  'mysql',
+    host:'database-1.cqwpdgcki6p6.ap-southeast-1.rds.amazonaws.com',
+    port:3306
+})
+
+
+
+const Customer = sequelize.define('customer', {
+    user_id: {
+        type:DataTypes.INTEGER,
+        primaryKey:true,
+        autoIncrement:true
+    },
+    username:{
+        type: DataTypes.STRING,
+        allowNull:false,
+        validate:{
+            len:[1,10]
+        },
+        // get() {
+        //     const rawValue = this.getDataValue('username')
+        //     return rawValue.toUpperCase()
+        // }
+    },
+    password:{
+        type: DataTypes.STRING,
+        // set(value) {
+        //     const salt = bcrypt.genSaltSync(12)
+        //     const hash = bcrypt.hashSync(value, salt)
+        //     this.setDataValue('password', hash)
+        // }
+    },
+    age: {
+        type: DataTypes.INTEGER,
+        defaultValue:21,
+    },
+    vegetarian:{
+        type: DataTypes.BOOLEAN,
+        defaultValue:true
+    },
+    description: {
+        type:DataTypes.STRING,
+        // set(value){
+        //     const compressed = zlib.deflateSync(value).toString('base64')
+        //     this.setDataValue('description', compressed)
+        // },
+        // get() {
+        //     const value = this.getDataValue('description')
+        //     const uncompressed = zlib.inflateSync(Buffer.from(value, 'base64'))
+        //     return uncompressed.toString()
+        // }    
+    },
+    aboutUser:{
+        type:DataTypes.VIRTUAL,
+        get(){
+            return this.username+this.description
+        }
+    },
+    email:{
+        type:DataTypes.STRING,
+        unique: true,
+        allowNull: true,
+        validate:{
+            myEmailValidator(value){
+                if(value === null){
+                    throw new Error("Please enter an email!")
+                }
+            }
+            
+        }
+    }    
+},
+{   
+    freezeTableName:true,
+    timestamps:true,
+    validate:{
+        myEmailValidator(value){
+            if(this.username == this.password) {
+                throw new Error("Please cannot be your username")
+            }else{
+                console.log('Motherfucker?')
+            }
+        }
+    },
+    // paranoid table : not really delete data, but put a column to note the data is deleted (but still in the database)
+    // timestamps : true -> to create the delete timestamps
+    paranoid:true,
+    // Changing the DeletedAt Column Name
+    deletedAt: 'timeDestoryed'
+});
+
+
+Customer.sync({ alter:true })
+.then(() => {
+
+
+    //  Restoring a Soft Deleted Record with Restore()
+    return Customer.restore({ 
+        where: {user_id:4}
+    })
+ 
+    // Executing (default): DELETE FROM `customer` WHERE `user_id` = 3
+1
+}).then((data) => {
+    console.log(data);
+}).catch((err) => {
+   console.log(err)
+})
