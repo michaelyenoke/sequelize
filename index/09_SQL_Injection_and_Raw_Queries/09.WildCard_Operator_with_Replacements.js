480b12ff3b994051a24c57a8999fc223b8cfd183
Y.H. Chen <michaelyenoke@hotmail.com>
2022-01-11 08:06:49 +0800

    Create 09.WildCard_Operator_with_Replacements.js

---
+++ index/09_SQL_Injection_and_Raw_Queries/09.WildCard_Operator_with_Replacements.js
@@ -0,0 +1,206 @@
+const Sequelize = require('sequelize')
+const bcrypt = require('bcrypt')
+const zlib = require('zlib')
+
+const { DataTypes, Op } = Sequelize
+
+const sequelize = new Sequelize('sequelize_project',  'michaelyenoke', '1qaz2wsx3edc', {
+    dialect:  'mysql',
+    host:'database-1.cqwpdgcki6p6.ap-southeast-1.rds.amazonaws.com',
+    port:3306
+})
+
+
+
+const Customer = sequelize.define('customer', {
+    user_id: {
+        type:DataTypes.INTEGER,
+        primaryKey:true,
+        autoIncrement:true
+    },
+    username:{
+        type: DataTypes.STRING,
+        allowNull:false,
+        validate:{
+            len:[1,10]
+        },
+        // get() {
+        //     const rawValue = this.getDataValue('username')
+        //     return rawValue.toUpperCase()
+        // }
+    },
+    password:{
+        type: DataTypes.STRING,
+        // set(value) {
+        //     const salt = bcrypt.genSaltSync(12)
+        //     const hash = bcrypt.hashSync(value, salt)
+        //     this.setDataValue('password', hash)
+        // }
+    },
+    age: {
+        type: DataTypes.INTEGER,
+        defaultValue:21,
+    },
+    vegetarian:{
+        type: DataTypes.BOOLEAN,
+        defaultValue:true
+    },
+    description: {
+        type:DataTypes.STRING,
+        // set(value){
+        //     const compressed = zlib.deflateSync(value).toString('base64')
+        //     this.setDataValue('description', compressed)
+        // },
+        // get() {
+        //     const value = this.getDataValue('description')
+        //     const uncompressed = zlib.inflateSync(Buffer.from(value, 'base64'))
+        //     return uncompressed.toString()
+        // }    
+    },
+    aboutUser:{
+        type:DataTypes.VIRTUAL,
+        get(){
+            return this.username+this.description
+        }
+    },
+    email:{
+        type:DataTypes.STRING,
+        unique: true,
+        allowNull: true,
+        validate:{
+            myEmailValidator(value){
+                if(value === null){
+                    throw new Error("Please enter an email!")
+                }
+            }
+            
+        }
+    }    
+},
+{   
+    freezeTableName:true,
+    timestamps:false,
+    // Model Wide Validation
+    // validate:{
+    //     myEmailValidator(value){
+    //         if(this.username == this.password) {
+    //             throw new Error("Please cannot be your username")
+    //         }else{
+    //             console.log('Motherfucker?')
+    //         }
+    //     }
+    // }
+});
+
+
+Customer.sync({ alter:true })
+.then(() => {
+    // Passing an Array to Replacements
+    return sequelize.query('SELECT * FROM customer WHERE username Like :username', {
+        replacements:{ username:'mi%'}
+    })
+    // Executing (default): SELECT * FROM customer WHERE username Like 'mi%'
+    // [
+    //   [
+    //     {
+    //       user_id: 6,
+    //       username: 'Mitt',
+    //       password: '$2b$12$igPOzVq2VbYkbgLZD1tf3Oe/7Hscx6Sp6oukCgWKKb9CFWfXu4Pnu',
+    //       age: 21,
+    //       vegetarian: 1,
+    //       description: null,
+    //       email: null
+    //     },
+    //     {
+    //       user_id: 14,
+    //       username: 'mike',
+    //       password: null,
+    //       age: 31,
+    //       vegetarian: 1,
+    //       description: null,
+    //       email: 'me@sfc.com'
+    //     },
+    //     {
+    //       user_id: 15,
+    //       username: 'mike',
+    //       password: null,
+    //       age: 31,
+    //       vegetarian: 1,
+    //       description: null,
+    //       email: 'meee@ccc.com'
+    //     }
+    //   ],
+    //   [
+    //     {
+    //       user_id: 6,
+    //       username: 'Mitt',
+    //       password: '$2b$12$igPOzVq2VbYkbgLZD1tf3Oe/7Hscx6Sp6oukCgWKKb9CFWfXu4Pnu',
+    //       age: 21,
+    //       vegetarian: 1,
+    //       description: null,
+    //       email: null
+    //     },
+    //     {
+    //       user_id: 14,
+    //       username: 'mike',
+    //       password: null,
+    //       age: 31,
+    //       vegetarian: 1,
+    //       description: null,
+    //       email: 'me@sfc.com'
+    //     },
+    //     {
+    //       user_id: 15,
+    //       username: 'mike',
+    //       password: null,
+    //       age: 31,
+    //       vegetarian: 1,
+    //       description: null,
+    //       email: 'meee@ccc.com'
+    //     }
+    //   ]
+    // ]
+}).then((data) => {
+    
+    // mysql,sql will show double result
+    // console.log(data) 
+
+    // to prevent double result in console
+    [results, metadata] = data
+    console.log(results)
+
+
+    //     Executing (default): SELECT * FROM customer WHERE username Like 'mi%'
+    // [
+    //   {
+    //     user_id: 6,
+    //     username: 'Mitt',
+    //     password: '$2b$12$igPOzVq2VbYkbgLZD1tf3Oe/7Hscx6Sp6oukCgWKKb9CFWfXu4Pnu',
+    //     age: 21,
+    //     vegetarian: 1,
+    //     description: null,
+    //     email: null
+    //   },
+    //   {
+    //     user_id: 14,
+    //     username: 'mike',
+    //     password: null,
+    //     age: 31,
+    //     vegetarian: 1,
+    //     description: null,
+    //     email: 'me@sfc.com'
+    //   },
+    //   {
+    //     user_id: 15,
+    //     username: 'mike',
+    //     password: null,
+    //     age: 31,
+    //     vegetarian: 1,
+    //     description: null,
+    //     email: 'meee@ccc.com'
+    //   }
+    // ]
+
+}).catch((err) => {
+   console.log(err)
+})
