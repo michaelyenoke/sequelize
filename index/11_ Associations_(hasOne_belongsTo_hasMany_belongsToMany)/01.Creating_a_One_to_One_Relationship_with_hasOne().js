858dc1c93af50c200f813236003a80e28bab3afd
Y.H. Chen <michaelyenoke@hotmail.com>
2022-01-11 13:24:36 +0800

    Update and rename 01.js to 01.Creating_a_One_to_One_Relationship_with_hasOne().js

---
+++ index/11_ Associations_(hasOne_belongsTo_hasMany_belongsToMany)/01.Creating_a_One_to_One_Relationship_with_hasOne().js
@@ -0,0 +1,47 @@
+const Sequelize = require('sequelize')
+const bcrypt = require('bcrypt')
+const zlib = require('zlib')
+const { HasOne } = require('sequelize')
+
+const { DataTypes, Op } = Sequelize
+
+const sequelize = new Sequelize('sequelize_project',  'michaelyenoke', '1qaz2wsx3edc', {
+    dialect:  'mysql',
+    host:'database-1.cqwpdgcki6p6.ap-southeast-1.rds.amazonaws.com',
+    port:3306
+})
+
+// table one : country
+const Country = sequelize.define('country', {
+    countryName:{
+        type:DataTypes.STRING,
+        unque:true
+    }
+},{
+    timestamps:false
+})
+
+// talbe two : capital
+const Capital = sequelize.define('capital',{
+    capitalName:{
+        type:DataTypes.STRING,
+        unique:true
+    }
+},{
+    timestamps: false
+})
+
+// parent table: Country table
+// foreign key: Capital table
+// You call the method on the parent table and pass it foreign table as argument.
+Country.hasOne(Capital);
+
+
+// use sequelize.sync (not Capital.sync or Country.sync) can make both table implemented to sql 
+sequelize.sync({alter:true}).then(()=> {
+        // working with our updated table.
+
+        
+}).catch((err) => {
+    console.log(err)
+})
