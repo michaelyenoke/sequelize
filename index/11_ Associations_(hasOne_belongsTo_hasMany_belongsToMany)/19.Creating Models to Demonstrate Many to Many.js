b154b3c245589c42607775a61ecc4e8cbbcf8e1a
Y.H. Chen <michaelyenoke@hotmail.com>
2022-01-12 09:54:26 +0800

    Create 19.Creating Models to Demonstrate Many to Many.js

---
+++ index/11_ Associations_(hasOne_belongsTo_hasMany_belongsToMany)/19.Creating Models to Demonstrate Many to Many.js
@@ -0,0 +1,42 @@
+const Sequelize = require('sequelize')
+const bcrypt = require('bcrypt')
+const zlib = require('zlib')
+const { HasOne } = require('sequelize')
+
+const { DataTypes, Op } = Sequelize
+
+const sequelize = new Sequelize('sequelize_project',  'michaelyenoke', '1qaz2wsx3edc', {
+    dialect:  'mysql',
+    host:'database-1.cqwpdgcki6p6.ap-southeast-1.rds.amazonaws.com',
+    port:3306
+})
+
+
+
+const Customer = sequelize.define('customer', {
+    customerName: {
+        type: DataTypes.STRING
+    }
+},{
+    timestamps: false
+})
+
+const Product = sequelize.define('product', {
+    productName: {
+       type:DataTypes.STRING
+    }
+},{
+    timestamps: false
+})
+
+Customer.belongsToMany(Product, { through: 'customerproduct' } )
+Product.belongsToMany(Customer, { through: 'customerproduct' } )
+
+// Executing (default): ALTER TABLE `customerproduct` CHANGE `createdAt` `createdAt` DATETIME NOT NULL;
+// Executing (default): ALTER TABLE `customerproduct` CHANGE `updatedAt` `updatedAt` DATETIME NOT NULL;
+// Executing (default): SHOW INDEX FROM `customerproduct`
+
+
+sequelize.sync({ alter:true }).then(() => {
+
+}).catch
